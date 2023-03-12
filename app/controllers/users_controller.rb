68e29830847b2bdacb0ff8da7a95aa10a815dd96
Denny Doan <denny-333@hotmail.com>
2023-03-11 21:32:24 -0500

    Login/sign up feature added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
 
 def new
 end
@@ -13,7 +14,7 @@ def create
   end
 end
 
-private
+
 
 def user_params
   params.require(:user).permit(:name, :email, :password, :password_confirmation)
